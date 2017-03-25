25fef503f15b229ae889d649456a2aabc2b64178
Richard Littauer <richard.littauer@gmail.com>
2017-03-24 22:42:04 -0400

    Fixed shell script

--- creat.sh
+++ creat.sh
@@ -2,17 +2,19 @@ FILES=static/img/*
 for f in $FILES
 do
   filename=$(basename "$f")
-  file="content/portfolio/"$filename
-  touch "$file"
-#   echo "+++
-# showonlyimage = true
-# draft = false
-# image = \"img/"$filename"\"
-# date = \"2016-11-05T18:25:22+05:30\"
-# title = \""$f"\"
-# weight = 0
-# +++
-#
-# And I would walk 10,000 miles.
-# " >> file
+  title=$(echo $filename | cut -d'.' -f1)
+  cleanfile="content/portfolio/"${title//[,)(]/}".md"
+  cleanfile="${cleanfile// /-}"
+  touch $cleanfile
+  echo "+++
+showonlyimage = true
+draft = false
+image = \"img/"$filename"\"
+date = \"2016-11-05T18:25:22+05:30\"
+title = \""$title"\"
+weight = 0
++++
+
+And I would walk 10,000 miles.
+" >> "$cleanfile"
 done
